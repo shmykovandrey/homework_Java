a3eda3438a1ad5d33d76073d1198a9004c23c70f
Shmykov Andrey <shmykovadrey88@gmail.com>
2024-10-12 23:49:50 +0300

    feat:task 14_2 complete

--- src/main/java/hw_14.java
+++ src/main/java/hw_14.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class hw_14 {
     public static void main(String[] args) {
-        List<String> colors = new ArrayList<>(List.of("White","Tan","Yellow","Orange","Red","Pink","Purple","Blue"));
-        System.out.println(colors);
-        for (int i=0; i<colors.size();i++){
-            if (colors.get(i).indexOf('l')!= -1) {
-                colors.remove(i);
-            }
+
+        List<Integer> myIntList = new ArrayList<>();
+        for (int i = 100; i <= 1000; i++) {
+            myIntList.add(i);
         }
-        System.out.println(colors);
+
+        System.out.println(myIntList);
+
     }
 }
